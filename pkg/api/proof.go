6952461f780812e3234593f1e30a860677e46a4d
Diego Gallegos <diegarcia91@gmail.com>
2025-03-02 13:31:36 +0000

    WIP: Circuit and merkle tree logic (#7)
    Co-authored-by: Diego Gallegos <garndieg@amazon.com>

--- pkg/api/proof.go
+++ pkg/api/proof.go
@@ -7,11 +7,14 @@ import (
 	"keyless-auth/domain"
 	"keyless-auth/repository"
 	"net/http"
+<<<<<<< HEAD
+=======
 
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
 	"github.com/consensys/gnark/frontend/cs/r1cs"
+>>>>>>> bbd94c8 (WIP: Circuit and merkle tree logic (#7))
 )
 
 type ProofRequest struct {
@@ -22,7 +25,7 @@ type ProofRequest struct {
 }
 
 type ProofResponse struct {
-	Proof *groth16.Proof `json:"proof"`
+	Proof []byte `json:"proof"`
 }
 
 type ProofHandler struct {
@@ -42,16 +45,56 @@ func (h *ProofHandler) GenerateProof(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+<<<<<<< HEAD
 	proof, err := circuit.CompileCircuit(domain.Proof{
 		Leaf:      req.Leaf,
 		Root:      req.Root,
 		Siblings:  req.Siblings,
 		Positions: req.Positions,
 	})
+=======
+	var ckt circuit.ZKAuthCircuit
+	r1cs, err := frontend.Compile(ecc.BN254.ScalarField(), r1cs.NewBuilder, &ckt)
+>>>>>>> bbd94c8 (WIP: Circuit and merkle tree logic (#7))
 	if err != nil {
 		http.Error(w, "Failed to compile circuit", http.StatusInternalServerError)
 		return
 	}
 
+<<<<<<< HEAD
+=======
+	pk, _, err := groth16.Setup(r1cs)
+	if err != nil {
+		http.Error(w, "Failed to generate keys", http.StatusInternalServerError)
+		return
+	}
+	assignment := circuit.ZKAuthCircuit{
+		Leaf:          frontend.Variable(req.Leaf),
+		Root:          frontend.Variable(req.Root),
+		ProofElements: make([]frontend.Variable, len(req.Siblings)),
+		ProofIndex:    frontend.Variable(req.Positions),
+	}
+
+	for i := 0; i < len(req.Siblings); i++ {
+		assignment.ProofElements[i] = frontend.Variable(req.Siblings[i])
+	}
+	assignment.ProofIndex = frontend.Variable(1)
+
+	witness, err := frontend.NewWitness(&assignment, ecc.BN254.ScalarField())
+	if err != nil {
+		http.Error(w, "Failed to create witness", http.StatusInternalServerError)
+		return
+	}
+
+	proof, err := groth16.Prove(r1cs, pk, witness)
+	if err != nil {
+		http.Error(w, "Failed to generate proof", http.StatusInternalServerError)
+		return
+	}
+
+>>>>>>> bbd94c8 (WIP: Circuit and merkle tree logic (#7))
+	var proofBytes []byte
+	proof.WriteTo(bytes.NewBuffer(proofBytes))
+	json.NewEncoder(w).Encode(ProofResponse{Proof: proofBytes})
 	json.NewEncoder(w).Encode(ProofResponse{Proof: proof})
 }
