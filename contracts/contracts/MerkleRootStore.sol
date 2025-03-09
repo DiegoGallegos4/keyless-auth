5da8ce45c7717e453389c148091b1394361b8e53
Haseeb Raja <has33braja@gmail.com>
2025-03-10 03:32:24 +0500

    chore: cleanup

--- contracts/contracts/MerkleRootStore.sol
+++ contracts/contracts/MerkleRootStore.sol
@@ -7,7 +7,6 @@ contract MerkleRootStore {
     event MerkleRootUpdated(bytes32 newMerkleRoot);
 
     function updateMerkleRoot(bytes32 newRoot) public {
-        // verified before updating
         merkleRoot = newRoot;
         emit MerkleRootUpdated(newRoot);
     }
